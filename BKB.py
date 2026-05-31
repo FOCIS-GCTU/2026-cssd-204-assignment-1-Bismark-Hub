70da9e9590799e55c3816654546d28a1a5b6e680
Bismark-Hub <2425400268@live.gctu.edu.gh>
2026-05-31 14:47:52 +0000

    Add files via upload

---
+++ BKB.py
@@ -0,0 +1,36 @@
+# File: BKB.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Bismark Kyei Boateng
+# STUDENT ID:  <2425400268>
+# Email: <boatengbismark186@gmail.com>
+# Grader: <Mr. Augustus Buckman>
+#
+# On my honor, Bismark Kyei Boateng, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials BKB in stylized large block letters (12x10 each),
+    # separated by large periods (4 asterisks), with 3-period column padding.
+
+    print()
+    print("...BKB")
+    print()
+    print("...B.K.B.")
+    print()
+    print("...BBBBBBBB...........KK......KK.........BBBBBBBB...........")
+    print("...BB......BB.........KK....KK...........BB......BB.........")
+    print("...BB......BB.........KK..KK.............BB......BB.........")
+    print("...BBBBBBBB...........KKKK...............BBBBBBBB...........")
+    print("...BBBBBBBB...........KKKK...............BBBBBBBB...........")
+    print("...BB......BB.........KK..KK.............BB......BB.........")
+    print("...BB......BB.........KK....KK...........BB......BB.........")
+    print("...BB......BB.........KK......KK.........BB......BB.........")
+    print("...BBBBBBBB......**...KK......KK....**...BBBBBBBB......**...")
+    print("...BBBBBBBB......**...KK......KK....**...BBBBBBBB......**...")
+    print()
+
+
+main()
